b7e9001cb5db66cf58575b3df8496e7e0e81fd9a
Terrell Strong <stront2@uw.edu>
2019-10-30 17:37:05 -0700

    hw4-final and hw5-final
    HW5 was completed in a new scene. Related files are in the assignment5 folder

---
+++ stront2-vr-project/Assets/Assignment5/DoorOpener.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    public GameObject player;
+    public float openDistance = 5;
+    public float openAngle = 80;
+    public float openSpeed = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null) {
+            float distance = Vector3.Distance(player.transform.position, transform.position);
+            if (distance <= openDistance) {
+                if (transform.eulerAngles.y < openAngle) {
+                    transform.eulerAngles += Vector3.up * openSpeed * Time.deltaTime;
+                    if (transform.eulerAngles.y > openAngle) {
+                        transform.eulerAngles = Vector3.up * openAngle;
+                    }
+                }
+            } else {
+                transform.eulerAngles = Vector3.zero;
+            }
+        }
+    }
+}
---
+++ stront2-vr-project/Assets/Assignment5/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assignment5
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerController : MonoBehaviour
+    {
+        public float speed = 5;
+        public Rigidbody rigid;
+
+        // Start is called before the first frame update
+        void Start() {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            rigid.MovePosition(transform.position + transform.TransformDirection(inputVector) * speed * Time.deltaTime);
+            /*
+            if (Input.GetKey(KeyCode.W)) {
+                rigid.MovePosition(transform.position + this.transform.forward * speed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.S)) {
+                rigid.MovePosition(transform.position + -this.transform.forward * speed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.A)) {
+                rigid.MovePosition(transform.position + -transform.right * speed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.D)) {
+                rigid.MovePosition(transform.position + transform.right * speed * Time.deltaTime);
+            }*/
+        }
+    }
+}
